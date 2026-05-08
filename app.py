2afa5b05a8a8475031a1cbde51bec32bd47bfd5a
Nicholas Okoro <54383553+Nickblaq@users.noreply.github.com>
2026-05-08 15:48:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def download_video(url: str, download_id: str, format_id: Optional[str], use_coo
 
     ydl_opts = {
         **BASE_OPTS,
-        "format": "b",
+       # "format": "b",
         "format_sort": [
             'vcodec:h264', 
             'res',      # Aim for 1080p specifically
